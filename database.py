4d26cb10932fbfd05baa7457eb4fe7e5357a2664
rafa-speres <rafa.scalon@gmail.com>
2024-06-19 11:47:04 -0300

    finalizacao database.py

--- database.py
+++ database.py
@@ -7,23 +7,3 @@ def conectar_bd():
         dsn="orclgrad1.icmc.usp.br:1521/pdb_elaine.icmc.usp.br"
     )
     return connection
-
-def cadastrar_usuario_bd(usuario, senha, tipo_usuario):
-    conn = conectar_bd()
-    cursor = conn.cursor()
-    try:
-        cursor.execute(
-            "INSERT INTO USERS (USUARIO, SENHA, TIPO_USUARIO) VALUES (:usuario, :senha, :tipo_usuario)",
-            [usuario, senha, tipo_usuario]
-        )
-        conn.commit()
-    except Exception as e:
-        print(f"Erro ao cadastrar usuário: {e}")
-    finally:
-        cursor.close()
-        conn.close()
-
-#TODO: param -- username, senha
-#return -- tipo, check (bool se existe ou nao o usuario)
-def validacao_user():
-    pass
